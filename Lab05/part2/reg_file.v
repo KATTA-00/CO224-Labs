1ff26aeb505c39588f09c9ff7818c6eda4cd83dc
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-05-08 22:01:57 +0530

    save

--- Lab05/part2/reg_file.v
+++ Lab05/part2/reg_file.v
@@ -24,27 +24,28 @@ module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK,
 
     // declare output ports
     output [7:0] OUT1, OUT2;
+    
     // declare registers to store output values
     reg [7:0] OUT1, OUT2;
     
     // declare the 8-bit, 8 registers
-    reg [7:0] register0, register1, register2, register3, register4, register5, register6, register7;
+    reg [7:0] registers [7:0];
 
     // always block for change the OUT1, in a change of OUT1ADDRESS or register values
     // with a 2 time unit delay
-    always @(OUT1ADDRESS, register0, register1, register2, register3, register4, register5, register6, register7) begin
+    always @(OUT1ADDRESS, registers[0],registers[1],registers[2],registers[3],registers[4],registers[5],registers[6],registers[7]) begin
         
         // selectt the appropriate reg numbers
         case(OUT1ADDRESS)
 
-            3'b000: #2 OUT1 = register0; // map register0 to OUT1
-            3'b001: #2 OUT1 = register1; // map register1 to OUT1
-            3'b010: #2 OUT1 = register2; // map register2 to OUT1
-            3'b011: #2 OUT1 = register3; // map register3 to OUT1
-            3'b100: #2 OUT1 = register4; // map register4 to OUT1
-            3'b101: #2 OUT1 = register5; // map register5 to OUT1
-            3'b110: #2 OUT1 = register6; // map register6 to OUT1
-            3'b111: #2 OUT1 = register7; // map register7 to OUT1
+            3'b000: #2 OUT1 = registers[0]; // map register0 to OUT1
+            3'b001: #2 OUT1 = registers[1]; // map register1 to OUT1
+            3'b010: #2 OUT1 = registers[2]; // map register2 to OUT1
+            3'b011: #2 OUT1 = registers[3]; // map register3 to OUT1
+            3'b100: #2 OUT1 = registers[4]; // map register4 to OUT1
+            3'b101: #2 OUT1 = registers[5]; // map register5 to OUT1
+            3'b110: #2 OUT1 = registers[6]; // map register6 to OUT1
+            3'b111: #2 OUT1 = registers[7]; // map register7 to OUT1
 
             default:  #2 OUT1 = 0; // default value 
 
@@ -54,19 +55,19 @@ module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK,
 
     // always block for chnage the OUT1, in a change of OUT2ADDRESS or register values
     // with a 2 time unit delay
-    always @(OUT2ADDRESS, register0, register1, register2, register3, register4, register5, register6, register7) begin
+    always @(OUT2ADDRESS, registers[0],registers[1],registers[2],registers[3],registers[4],registers[5],registers[6],registers[7]) begin
         
         // selectt the appropriate reg numbers
         case(OUT2ADDRESS)
 
-            3'b000: #2 OUT2 = register0; // map register0 to OUT2
-            3'b001: #2 OUT2 = register1; // map register1 to OUT2
-            3'b010: #2 OUT2 = register2; // map register2 to OUT2
-            3'b011: #2 OUT2 = register3; // map register3 to OUT2
-            3'b100: #2 OUT2 = register4; // map register4 to OUT2
-            3'b101: #2 OUT2 = register5; // map register5 to OUT2
-            3'b110: #2 OUT2 = register6; // map register6 to OUT2
-            3'b111: #2 OUT2 = register7; // map register7 to OUT2
+            3'b000: #2 OUT2 = registers[0]; // map register0 to OUT2
+            3'b001: #2 OUT2 = registers[1]; // map register1 to OUT2
+            3'b010: #2 OUT2 = registers[2]; // map register2 to OUT2
+            3'b011: #2 OUT2 = registers[3]; // map register3 to OUT2
+            3'b100: #2 OUT2 = registers[4]; // map register4 to OUT2
+            3'b101: #2 OUT2 = registers[5]; // map register5 to OUT2
+            3'b110: #2 OUT2 = registers[6]; // map register6 to OUT2
+            3'b111: #2 OUT2 = registers[7]; // map register7 to OUT2
 
             default:  #2 OUT2 = 0; // default value 
 
@@ -85,16 +86,16 @@ module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK,
             // select the appropriate reg numbers
             case(INADDRESS)
 
-                3'b000: #1 register0 = IN; // write IN to register0
-                3'b001: #1 register1 = IN; // write IN to register1
-                3'b010: #1 register2 = IN; // write IN to register2
-                3'b011: #1 register3 = IN; // write IN to register3
-                3'b100: #1 register4 = IN; // write IN to register4
-                3'b101: #1 register5 = IN; // write IN to register5
-                3'b110: #1 register6 = IN; // write IN to register6
-                3'b111: #1 register7 = IN; // write IN to register7
+                3'b000: #1 registers[0] = IN; // write IN to register0
+                3'b001: #1 registers[1] = IN; // write IN to register1
+                3'b010: #1 registers[2] = IN; // write IN to register2
+                3'b011: #1 registers[3] = IN; // write IN to register3
+                3'b100: #1 registers[4] = IN; // write IN to register4
+                3'b101: #1 registers[5] = IN; // write IN to register5
+                3'b110: #1 registers[6] = IN; // write IN to register6
+                3'b111: #1 registers[7] = IN; // write IN to register7
                 
-                default #1 register0 = 0; // default value
+                default #1 registers[0] = IN; // default value
 
             endcase
         end
@@ -102,14 +103,14 @@ module reg_file(IN, OUT1, OUT2, INADDRESS, OUT1ADDRESS, OUT2ADDRESS, WRITE, CLK,
         // if RESET is enable
         // set all the register values to 0
         if (RESET) begin
-            register0 = 0;
-            register1 = 0;
-            register2 = 0;
-            register3 = 0;
-            register4 = 0;
-            register5 = 0;
-            register6 = 0;
-            register7 = 0;
+            registers[0] = 0; // 0 to register0
+            registers[1] = 0; // 0 to register1
+            registers[2] = 0; // 0 to register2
+            registers[3] = 0; // 0 to register3
+            registers[4] = 0; // 0 to register4
+            registers[5] = 0; // 0 to register5
+            registers[6] = 0; // 0 to register6
+            registers[7] = 0; // 0 to register7
         end
 
     end
