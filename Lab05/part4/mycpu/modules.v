aa639fed210fd0cc0d7e32abc6127e9479d9e568
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-05-25 09:31:31 +0530

    save

--- Lab05/part4/mycpu/modules.v
+++ Lab05/part4/mycpu/modules.v
@@ -1,53 +1,66 @@
+// CO224 - Lab05 PART-3
+// GROUP - 11
+
+// this file contains two's compliment unit and mux unit
+// two_comp - get a 8 bit data and output its 2's compliment
+// mux - used to select two 8 bit data with a selection bit
+
+// module for get the 2's complwment required for sub instruction
 module two_comp(DATA, OUT);
 
     input [7:0] DATA;
     output [7:0] OUT;
 
+    //assign 2's complement of the data to thw output
     assign #1 OUT = ~DATA + 1;
 
 endmodule
 
-
+//2x1 8-bit mux module for transfer data
 module mux_8(DATA1, DATA2, SELECT, OUTPUT);
 
-    input [7:0] DATA1, DATA2;
-    input SELECT;
-    output reg [7:0] OUTPUT;
+    input [7:0] DATA1, DATA2; // input data to the mux
+    input SELECT; // selector bit
+    output reg [7:0] OUTPUT; // output from the mux
 
     always @(DATA1, DATA2, SELECT) begin 
 
-        if (SELECT) 
+        if (SELECT) //select= 1, data 2 is selected from the mux
             OUTPUT = DATA2;
-        else
+        else // else data 1 is selected
             OUTPUT = DATA1;
 
     end
 
 endmodule
 
+//2x1 32-bit mux module for transfer data
 module mux_32(DATA1, DATA2, SELECT, OUTPUT);
 
-    input [31:0] DATA1, DATA2;
-    input SELECT;
-    output reg [31:0] OUTPUT;
+    input [31:0] DATA1, DATA2; // input data to the mux
+    input SELECT; // selector bit
+    output reg [31:0] OUTPUT; // output from the mux
 
     always @(DATA1, DATA2, SELECT) begin 
 
-        if (SELECT) 
+        if (SELECT) //select= 1, data 2 is selected from the mux
             OUTPUT = DATA2;
-        else
+        else // else data 1 is selected
             OUTPUT = DATA1;
 
     end
 
 endmodule
 
+// adder to add the offset of the jump/branch instruction
 module pc_adder(DATA1, DATA2, RESULT);
 
+    // declare the ports
     input signed [31:0] DATA1;
     input signed [7:0] DATA2;
     output [31:0] RESULT;
 
+    // assign the added value
     assign #2 RESULT = DATA1 + DATA2 * 4;
 
 endmodule
--- Lab05/part4/mycpu/pc.v
+++ Lab05/part4/mycpu/pc.v
@@ -1,17 +1,23 @@
+// CO224 - Lab05 PART-3
+// GROUP - 11
 
+// program counter module
 module pc(PC_TO, RESET, CLK, PC, PC_NEXT);
 
     input RESET, CLK;
     input [31:0] PC_TO;
-    output reg [31:0] PC;
+    output reg [31:0] PC;// reg to hold the address of the current executing instruction
     
-    output [31:0] PC_NEXT;
-    wire [31:0] adder_out;
+    output [31:0] PC_NEXT;// next instruction to be executed
+    wire [31:0] adder_out;// address of the next instruction to be executed
 
+    // output from the PC adder (adderout = PC+4)
     pc_add pc_adder(PC, PC_NEXT);
 
     always @(posedge CLK) begin
-
+        // at every positive edge of the clock
+        // if reset is enabled, 0 is written to PC
+        // else Next address is written to PC 
         if (RESET)
             #1 PC = 32'b00000000000000000000000000000000;
         else
@@ -21,11 +27,14 @@ module pc(PC_TO, RESET, CLK, PC, PC_NEXT);
 endmodule
 
 
+//pc adder module (adder_out = PC+4(because word size is 4 bytes))
 module pc_add(PC, adder_out);
 
+    // declare ports
     input [31:0] PC;
     output [31:0] adder_out;
 
+    // Incremtn the value by 4 and assing it the output with a delay of 1
     assign #1 adder_out = PC + 4 ;
 
 endmodule
